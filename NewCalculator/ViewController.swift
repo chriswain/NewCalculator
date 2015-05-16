b8a7ca0a521c84bfbc044899e2ca93dafe6add1a
chriswain <chriswainwrightaaron@gmail.com>
2015-05-16 11:09:43 -0400

    update 1 for new calculator

--- NewCalculator/ViewController.swift
+++ NewCalculator/ViewController.swift
@@ -27,5 +27,49 @@ class ViewController: UIViewController {
        
         println("digit = \(digit)")
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userTypingNumber {
+            enter()
+        }
+        
+        switch operation {
+        case "×": performOperation { $1 * $0 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $1 + $0 }
+        case "−": performOperation { $1 - $0 }
+        default: break
+        }
+        
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    
+    var operandStack: [Double] = []
+   
+    @IBAction func enter() {
+        userTypingNumber = false
+        operandStack.append(displayValue)
+        println("operand stack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+          return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userTypingNumber = false
+        }
+    }
+    
 }
 
